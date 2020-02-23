0a3b110d7acf2e5f791970961ccfa17e7df6b713
Jessica <jessica_arq_urb@hotmail.com>
2020-02-23 19:13:41 -0300

    adicionado toast pdf git

--- app/src/main/java/com/example/aula_02_exercicio_internacionalizacao/MainActivity.kt
+++ app/src/main/java/com/example/aula_02_exercicio_internacionalizacao/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.aula_02_exercicio_internacionalizacao
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, "Versão final", Toast.LENGTH_SHORT).show()
     }
 }
